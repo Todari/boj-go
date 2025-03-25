eda4efc3a40638993c497e097998e6909f06e902
이태훈 <rhymint@gmail.com>
2025-03-25 15:58:31 +0900

    solve 15666

---
+++ codes/15666/main.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n")
+const [N, M] = input[0].split(" ").map(Number)
+const arr = input[1].split(" ").map(Number).sort((a,b) => a - b)
+
+function getPermutation(arr, select) {
+  if (select === 1) return arr.map(v => [v])
+
+  let result = [];
+  arr.forEach((v, i, o) => {
+    let fixed = v
+    let rest = o.slice(i)
+    let next = getPermutation(rest, select - 1)
+    let attached = next.map((value) => [fixed, ...value])
+    result.push(...attached)
+  })
+
+  return result
+}
+
+let set = new Set(getPermutation(arr, M).map(v => v.join(" ")))
+console.log(Array.from(set).join("\n"))
